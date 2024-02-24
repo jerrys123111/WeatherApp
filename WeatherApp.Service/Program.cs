aae7e0d74089f288693ddaa69c5b9738b8a828c0
jerrys123111 <jerrys123111@gmail.com>
2024-02-24 16:40:28 -0500

    Move console app portion into its own project

---
+++ WeatherApp.Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    /// <summary>
+    /// Main method called by runtime at application startup time.
+    /// </summary>
+	public static void Main(string[] args)
+	{
+
+        var host = CreateBuilder(args);
+
+        host.Run();
+
+	}
+
+    /// <summary>
+    /// Initializes a static host to be executed.
+    /// This adds all the services and other information for the application to start up.
+    /// </summary>
+    /// <param name="args">Configuration settings for the <see cref="IHostApplicationBuilder"/></param>
+    /// <returns><see cref="IHost"/></returns>
+    public static IHost CreateBuilder(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddHttpClient();
+        builder.Metrics.Services.AddMetrics();
+
+        return builder.Build();
+    }
+    
+}
